688c5002d8870ba56696134a7c7210fc0784b16f
Eugene Chernov <ageent.10001@gmail.com>
2020-09-19 12:45:16 +0300

    Error
    "select :fieldName from Tax group by :fieldName" isn't worked.
    "select a from Tax group by a" is worked.

--- src/main/java/ru/mycompany/test202001/repositories/CustomTaxRepositoryImpl.java
+++ src/main/java/ru/mycompany/test202001/repositories/CustomTaxRepositoryImpl.java
@@ -1,10 +1,11 @@
 package ru.mycompany.test202001.repositories;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.mycompany.test202001.domain.Tax;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
@@ -16,8 +17,21 @@ public class CustomTaxRepositoryImpl implements CustomTaxRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    public CustomTaxRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public List<String> findUniqueValuesOfField(String fieldName) {
-        
+        TypedQuery<String> query = entityManager.createQuery(
+                "select :fieldName from Tax group by :fieldName",
+                String.class
+        );
+        query.setParameter("fieldName", fieldName);
+        /*TypedQuery<String> query = entityManager.createQuery(
+                "select a from Tax group by a",
+                String.class
+        );*/
+        return query.getResultList();
     }
 }
